25c3a274770be119b5ad002c71a0c89a41f358b1
Bonnet1 <c.mannion84@gmail.com>
2019-10-08 19:19:07 -0400

    Modified files to create backend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+SurveyApp::Application.routes.draw do
+  resources :surveys do
+    get 'answers', on: :member
+  end
+  resources :participants
 end
